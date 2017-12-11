ba247cece0d7573f943492b0165b869e662ee4a5
dierodfer <32866881+dierodfer@users.noreply.github.com>
2017-12-11 14:54:54 +0100

    incluido boostrap y plantilla

--- Milestone 3/Proyecto base/portal-votaciones-master/portal-votaciones.py
+++ Milestone 3/Proyecto base/portal-votaciones-master/portal-votaciones.py
@@ -1,11 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def main():
+    return 'Hello world!'
+
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
 
 
 if __name__ == '__main__':
